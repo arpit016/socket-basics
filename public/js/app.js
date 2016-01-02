a0d66c3210c724690793ae2b9c1585f2dff5ac34
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-01-03 02:41:04 +0530

    Added timestamp and display messages

--- public/js/app.js
+++ public/js/app.js
@@ -5,5 +5,20 @@ socket.on('connect', function () {
 });
 
 socket.on('message', function(message) {
+	var momenttimestamp = moment.utc(message.timestamp);
 	console.log("New message:", message.text);
+
+	jQuery('.messages').append("<p><strong>"+ momenttimestamp.local().format('h:mm a') + ": </strong>" + message.text + "</p>");
+});
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	socket.emit('message', {
+		text: $form.find('input[name=message]').val()
+	});
+	$form.find('input[name=message]').val("")
 });
